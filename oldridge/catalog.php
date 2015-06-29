6533b6c545ec56016fc0c9fd8f562bdb93f9f3ed
drspiderwee <@drspiderwee>
2015-06-29 18:50:48 +0300

    caroline finished

---
+++ oldridge/catalog.php
@@ -0,0 +1,122 @@
+<div class="row">
+	<div class="col-sm-12">
+		<div class="row">
+			<div class="col-sm-5"> <h1>Original Series</h1> </div>
+			<div class="col-sm-2"><h3>or</h3></div>
+			<div class="col-sm-5 right-text"><a href=""><h1 id="target">Black Edition</h1></a></div>
+		</div>
+	</div>
+</div>
+
+</br>
+<div class="row catalog">
+	<div class="col-sm-12">
+		<div class="row">
+			<div class="col-sm-4"><a href="#Deadly_Forest" data-toggle="modal"  >
+				<img src="img/lot/Deadly_Forest.jpg" alt="" class="img-responsive"></img>
+			</a>
+				<div class="caption">
+						<h3>Deadly Forest</h3>
+					
+						<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>
+			</div>
+			
+			<div class="col-sm-4"><a href="#Jet_Cat" data-toggle="modal">
+				<img src="img/lot/Jet_Cat.jpg" alt="" class="img-responsive"></img>
+			</a>
+				<div class="caption">
+					<h3>Jet Cat</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>
+			</div>
+
+			<div class="col-sm-4"><a href="#Mask" data-toggle="modal">
+					<img src="img/lot/Mask.jpg" alt="" class="img-responsive"></img></a>
+				<div class="caption">
+					<h3>Mask</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>
+			</div>
+
+			<div class="col-sm-4"><a href="#Awe" data-toggle="modal">
+					<img src="img/lot/Awe.jpg" alt="" class="img-responsive"></img></a>
+					<div class="caption">
+						<h3>Awe</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>
+			</div>
+
+			<div class="col-sm-4"><a href="#Crows" data-toggle="modal">
+				<img src="img/lot/Crows.jpg" alt="" class="img-responsive"></img></a>
+				<div class="caption">
+						<h3>Crows</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>
+			</div>
+
+			<div class="col-sm-4"><a href="#Cyclo-Eagle" data-toggle="modal">
+				<img src="img/lot/Cyclo-Eagle.jpg" alt="" class="img-responsive"></img></a>
+				<div class="caption">
+						<h3>Cyclo-Eagle</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>	
+			</div>
+
+			<div class="col-sm-4"><a href="#" data-toggle="modal">
+				<img src="img/lot/match_spirit.jpg" alt="" class="img-responsive"></img></a>
+				<div class="caption">
+						<h3>Match Spirit</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>	
+			</div>
+
+			<div class="col-sm-4"><a href="#" data-toggle="modal">
+				<img src="img/lot/Piggy.jpg" alt="" class="img-responsive"></img></a>
+				<div class="caption">
+						<h3>Piggy</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>	
+
+			</div>
+			
+			<div class="col-sm-4"><a href="#" data-toggle="modal">
+				<img src="img/lot/Red_Siluet.jpg" alt="" class="img-responsive"></img></a>
+				<div class="caption">
+						<h3>Red Siluet</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>	
+			</div>
+			
+			<div class="col-sm-4"><a href="#" data-toggle="modal">
+				<img src="img/lot/Shark.jpg" alt="" class="img-responsive"></img></a>
+				<div class="caption">
+						<h3>Shark</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>
+			</div>
+
+			<div class="col-sm-4"><a href="#" data-toggle="modal">
+				<img src="img/lot/Weird_Love.jpg" alt="" class="img-responsive"></img></a>
+				<div class="caption">
+						<h3>Weird Love</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>
+			</div>
+			
+			<div class="col-sm-4"><a href="#" data-toggle="modal">
+				<img src="img/lot/whale.jpg" alt="" class="img-responsive"></img></a>
+				<div class="caption">
+						<h3>Whale</h3>
+					<p><a href="#nuan" data-toggle="modal" class="btn btn-primary" role="button">Read more</a></p>
+				</div>
+			</div>
+
+
+
+
+
+
+		</div><!--ROW-->
+	</div><!--col-sm-12-->
+</div><!--ROW-->
---
+++ oldridge/nav.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-default" role="navigation">
+			  <div class="container">
+				    <!-- Brand and toggle get grouped for better mobile display -->
+				    <div class="navbar-header">
+				    	<!--<a class="navbar-brand"href="#">oldridge</a>-->
+
+				      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+				    </div>
+
+				    <!-- Collect the nav links, forms, and other content for toggling -->
+				    <div class="collapse navbar-collapse" id="navbar">
+				      <ul class="nav navbar-nav">
+				        <li><a href="index.php?view=start_page">Home</a></li>
+
+				        <li><a href="index.php?view=catalog">Catalog</a></li>
+
+						<li><a href="index.php?view=about">About</a></li>
+				        <li><a href="index.php?view=contact">Contact</a></li>
+				        <li><a href="index.php?view=blog">Blog</a></li>
+				      </ul>
+				    </div><!-- end navbar-collapse -->
+			  </div><!-- end container-fluid -->
+
+</nav>
